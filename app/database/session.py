12a3a53b162cea5df535b103cf91e85e794c8a7f
Puslore <mini.big10@mail.ru>
2025-05-08 18:53:17 +0800

    session file

--- app/database/session.py
+++ app/database/session.py
@@ -1,1 +1,22 @@
 # Файл, описывающий сессию с базой данных
+from contextlib import contextmanager
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+engine = create_engine('')
+Session = sessionmaker(bind=engine)
+
+@contextmanager
+def get_session():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+        
+    except Exception as err:
+        session.rollback()
+        raise err
+    
+    finally:
+        session.close()
