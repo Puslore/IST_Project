e1cbd69f44ec6dc7ac2353872aca59bc5187c954
Puslore <mini.big10@mail.ru>
2025-05-14 04:40:51 +0800

    bot controller update

--- app/controllers/bot_controller.py
+++ app/controllers/bot_controller.py
@@ -23,17 +23,61 @@ class BotController:
             BotController.active_auth_codes[code] = user_id
         else:
             while code in BotController.active_auth_codes.keys():
-                code = generate_auth_code
+                code = generate_auth_code()
             BotController.active_auth_codes[code] = user_id
         
         return code
     
-    async def verify_auth_code(self, checking_code: int, telegram_chat_id: int) -> bool:
+    def remove_verify_code(self, code: int):
+        try:
+            del self.active_auth_codes[code]
+            return True
+        
+        except KeyError:
+            return False
+    
+    async def verify_auth_code(self, checking_code: int, tg_chat_id: int) -> bool:
         '''
-        Проверяет авторизационный код и связывает Telegram ID с пользователем
+        Проверяет авторизационный код при подключении бота
         
         Args:
-            checking_code (int): Код, введенный пользователем в боте
+            checking_code (int): Код, введенный пользователем в интерфейсе
+            tg_chat_id (int): ID чата пользователя в Telegram
+            
+        Returns:
+            bool: True, если код верный и пользователuserь авторизован, иначе False
+        '''
+        try:
+            if isinstance(checking_code, str):
+                checking_code = int(checking_code)
+                
+            with get_session() as session:
+                print(f'{checking_code=}')
+                print(f'{self.active_auth_codes[checking_code]=}')
+                user = UserRepository(session).find_by_id(self.active_auth_codes[checking_code])
+                if checking_code in BotController.active_auth_codes:
+                    try:
+                        UserRepository(session).update(user.id, **{'tg_chat_id': tg_chat_id})
+                        print(f'Added new tg chat id')
+                        return True
+                    
+                    except Exception as err:
+                        print(f'Ошибка при добавлении chat id в БД - {err}')
+         
+        except ValueError:
+            print(f'Ошибка: Код "{checking_code}" не является числом')
+            return False
+        
+        except Exception as err:
+            print(f'Ошибка при проверке кода: {err}')
+            return False
+
+    async def send_login_code(self, checking_code: int, telegram_chat_id: int) -> bool:
+        '''
+        Проверяет авторизационный код при логине
+        
+        Args:
+            checking_code (int): Код, введенный пользователем в интерфейсе
             telegram_chat_id (int): ID чата пользователя в Telegram
             
         Returns:
@@ -43,18 +87,11 @@ class BotController:
             if isinstance(checking_code, str):
                 checking_code = int(checking_code)
 
-            user_id = BotController.active_auth_codes[checking_code]
-            print(f'{checking_code=}')
-            if checking_code in BotController.active_auth_codes:
-                return True
-            
-            # TODO добавление chat id пользователя в БД
-            # with get_session() as session:
-            # Удаление использованного кода из общего словаря
-            del BotController.active_auth_codes[checking_code]
-                
-            #     return True
-                
+            # user_id = BotController.active_auth_codes[checking_code]
+            print(f'LOGIN CODE - {checking_code}')
+
+            await self.send_notification(telegram_chat_id, str(checking_code))
+
         except ValueError:
             print(f'Ошибка: Код "{checking_code}" не является числом')
             return False
@@ -73,17 +110,23 @@ class BotController:
         '''
         return cls.active_auth_codes
     
-    async def send_notification(self, user_id: int, message: str) -> bool:
+    async def send_notification(self, tg_chat_id: int, message: str) -> bool:
         '''
         Отправляет уведомление пользователю через бота
         
         Args:
-            user_id (int): ID пользователя в базе данных
+            tg_chat_id (int): Telegram chat id пользователя
             message (str): Текст сообщения
             
         Returns:
             bool: True, если сообщение отправлено, иначе False
         '''
+        from app.bot.bot import bot
+        
+        print(f'Sending notification to {tg_chat_id=}, message={message[:5]}')
+        
+        await bot.send_message(tg_chat_id, message)
+        
         # TODO получение chat id пользователя из БД
         # with get_session() as session:
         #     user_repo = UserRepository(session)
